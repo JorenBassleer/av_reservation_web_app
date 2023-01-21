304973b9d1ca87972a36c02d5ea65f3fd4cc1996
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-01-21 22:38:14 +0100

    pre enge chap commit

--- src/components/tests/TheNavBar.test.js
+++ src/components/tests/TheNavBar.test.js
@@ -3,6 +3,7 @@
  */
 import { mount, RouterLinkStub  } from '@vue/test-utils';
 import { createStore } from 'vuex';
+import { vi } from 'vitest';
 import TheNavBar from '../../layouts/TheNavBar.vue';
 
 describe('Appliance.vue', () => {
@@ -20,12 +21,26 @@ describe('Appliance.vue', () => {
     });
   });
   it('Test home router-link', () => {
-      expect(wrapper.findAllComponents(RouterLinkStub)[0].props().to).toBe('/');
+    expect(wrapper.findAllComponents(RouterLinkStub)[0].props().to).toBe('/');
   });
   it('Test apparaten router-link', () => {
     expect(wrapper.findAllComponents(RouterLinkStub)[1].props().to).toBe('/apparaten');
   });
   it('Test mijn reservatie router-link', () => {
     expect(wrapper.findAllComponents(RouterLinkStub)[2].props().to).toBe('/mijn-reservatie');
+  });
+  it('#appliances-number has total amount of appliances in reservation', () => {
+    let wrapper = mount(TheNavBar, {
+      global: {
+        computed: { amountOfAppliances: 60 },
+        plugins: [store],
+        stubs: { RouterLink: RouterLinkStub },
+      },
+    });
+    const applianceNumberSpan = wrapper.find('#appliances-number');
+    if (expect(applianceNumberSpan.find('span').exists()).toBe(true)) {
+      console.log('here');
+      expect(applianceNumberSpan.text()).toBe(60);
+    }
   });
 });
