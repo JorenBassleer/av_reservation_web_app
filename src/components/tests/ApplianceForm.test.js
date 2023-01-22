4e310c575e4eee7d8731bddd9da16b3ae5a49886
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-01-22 20:55:03 +0100

    almost finished appliance form test

--- src/components/tests/ApplianceForm.test.js
+++ src/components/tests/ApplianceForm.test.js
@@ -1,4 +1,6 @@
-import { describe, it, beforeEach } from 'vitest';
+import {
+  describe, it, beforeEach, vi, expect,
+} from 'vitest';
 import { shallowMount } from '@vue/test-utils';
 import axios from 'axios';
 import { createStore } from 'vuex';
@@ -7,12 +9,18 @@ import ApplianceForm from '../appliances/ApplianceForm.vue';
 
 describe('ApplianceForm.vue', () => {
   const testAppliance = {
-    name: '',
-    details: '',
-    brand_id: '',
-    type_id: '',
-    storage: '0',
-    img_url: '',
+    data: {
+      data: [
+        {
+          name: 'test-appliance name',
+          details: 'test-appliance details',
+          brand_id: 1,
+          type_id: 2,
+          storage: 5,
+          img_url: 'google.com',
+        },
+      ],
+    },
   };
   let store;
   let wrapper;
@@ -28,7 +36,21 @@ describe('ApplianceForm.vue', () => {
   });
   // Test succcessful form submit
   it('Successful form post', async () => {
-
+    // Arrange
+    const [nameInput, detailsInput, imgUrlInput, storageInput] = wrapper.findAll('input');
+    await nameInput.setValue(testAppliance.data.data[0].name);
+    console.log('data val: ', await nameInput.setValue(testAppliance.data.data[0].name));
+    await detailsInput.setValue(testAppliance.data.data[0].details);
+    await imgUrlInput.setValue(testAppliance.data.data[0].img_url);
+    await storageInput.setValue(testAppliance.data.data[0].storage);
+    const axiosSpy = vi.spyOn(axios, 'post').mockReturnValue(testAppliance);
+    const fnSpy = vi.spyOn(wrapper.vm, 'handleSubmit');
+    // Act
+    console.log('wrapper find: ', storageInput.text());
+    await wrapper.find('button').trigger('click');
+    // Assert
+    expect(fnSpy).toHaveBeenCalledTimes(1);
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
   });
   // Test failed form submit
 });
