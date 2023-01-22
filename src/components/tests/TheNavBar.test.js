9ddcce89464c38702359c622f2014bc1d374eb49
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-01-22 13:34:52 +0100

    added test for total # in reservation in navbar

--- src/components/tests/TheNavBar.test.js
+++ src/components/tests/TheNavBar.test.js
@@ -1,16 +1,20 @@
 /**
  * @vitest-environment
  */
-import { mount, RouterLinkStub  } from '@vue/test-utils';
+import { mount, RouterLinkStub, shallowMount  } from '@vue/test-utils';
 import { createStore } from 'vuex';
-import { vi } from 'vitest';
+import { vi,  } from 'vitest';
 import TheNavBar from '../../layouts/TheNavBar.vue';
 
 describe('Appliance.vue', () => {
   let store;
   let wrapper;
   beforeEach(() => {
-    store = createStore();
+    store = createStore({
+      getters: {
+        getTotalAmountOfAppliancesInReservation() { return 60 }
+      }
+    });
     wrapper = mount(TheNavBar, {
       global: {
         plugins: [store],
@@ -29,18 +33,18 @@ describe('Appliance.vue', () => {
   it('Test mijn reservatie router-link', () => {
     expect(wrapper.findAllComponents(RouterLinkStub)[2].props().to).toBe('/mijn-reservatie');
   });
-  it('#appliances-number has total amount of appliances in reservation', () => {
-    let wrapper = mount(TheNavBar, {
+  it('#appliances-number has total amount of appliances in reservation', async () => {
+    // Arrange
+    const wrapper = mount(TheNavBar, {
       global: {
-        computed: { amountOfAppliances: 60 },
         plugins: [store],
         stubs: { RouterLink: RouterLinkStub },
       },
     });
-    const applianceNumberSpan = wrapper.find('#appliances-number');
-    if (expect(applianceNumberSpan.find('span').exists()).toBe(true)) {
-      console.log('here');
-      expect(applianceNumberSpan.text()).toBe(60);
-    }
+    // Act
+    const applianceNumber = parseInt(wrapper.find('#appliances-number').text(), 10);
+    // 60 like we defined in our mock store
+    // Assert
+    expect(applianceNumber).toBe(60);
   });
 });
