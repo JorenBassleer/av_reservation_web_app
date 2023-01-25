488ba5c9ac7507c3f6654951f32483c77d05861b
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-01-25 21:06:00 +0100

    expanded form post test, still not working tho

--- src/components/tests/ApplianceForm.test.js
+++ src/components/tests/ApplianceForm.test.js
@@ -6,12 +6,14 @@ import axios from 'axios';
 import { createStore } from 'vuex';
 import { useRouter } from 'vue-router';
 import ApplianceForm from '../appliances/ApplianceForm.vue';
+import baseAPIUrl from '../../composables/globals';
 
 describe('ApplianceForm.vue', () => {
   const testAppliance = {
     data: {
       data: [
         {
+          id: 1,
           name: 'test-appliance name',
           details: 'test-appliance details',
           brand_id: 1,
@@ -25,6 +27,15 @@ describe('ApplianceForm.vue', () => {
   let store;
   let wrapper;
   let router;
+  const mockRoute = {
+    name: 'view-appliance',
+    params: {
+      id: testAppliance.data.data[0].id,
+    },
+  };
+  const mockRouter = {
+    push: vi.fn(),
+  };
   beforeEach(() => {
     store = createStore({
       state: {
@@ -39,6 +50,10 @@ describe('ApplianceForm.vue', () => {
     wrapper = mount(ApplianceForm, {
       global: {
         plugins: [store, router],
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter,
+        },
       },
     });
   });
@@ -50,14 +65,28 @@ describe('ApplianceForm.vue', () => {
     await detailsInput.setValue(testAppliance.data.data[0].details);
     await imgUrlInput.setValue(testAppliance.data.data[0].img_url);
     await storageInput.setValue(testAppliance.data.data[0].storage);
+    // Set id's since they are not inputs
+    wrapper.vm.appliance.brand_id = testAppliance.data.data[0].brand_id;
+    wrapper.vm.appliance.type_id = testAppliance.data.data[0].type_id;
     vi.spyOn(axios, 'post').mockReturnValue(testAppliance);
+    const submitSpy = vi.spyOn(wrapper.vm, 'handleSubmit');
     // Act
     await wrapper.find('button').trigger('click');
-    // Assert
-    expect(wrapper.vm.handleSubmit()).toHaveBeenCalledTimes(1);
-    expect(axios.post).toHaveBeenCalledTimes(1);
-    // Check if store dispatch getAppliances gets called
-    // Check if router get pushed
+    wrapper.vm.$nextTick(() => {
+      // Assert
+      expect(submitSpy).toHaveBeenCalledTimes(1);
+      expect(axios.post).toHaveBeenCalledWith(`${baseAPIUrl}appliances`, testAppliance);
+      // Check if store dispatch getAppliances gets called
+      expect(store.actions.getAppliance()).toHaveBeenCalledTimes(1);
+      // Check if router get pushed
+      expect(mockRouter.push).toHaveBeenCalledTimes(1);
+      expect(mockRouter.push).toHaveBeenCalledWith({
+        name: 'view-appliance',
+        params: {
+          id: testAppliance.data.data[0].id,
+        },
+      });
+    });
   });
   // Test failed form submit
   it('Fail submit sets error val', async () => {
