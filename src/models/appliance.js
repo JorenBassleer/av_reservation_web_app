3f9e15bd356f43da5185f323bfed1aaa39379268
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-02-06 21:36:00 +0100

    added appliance model record tests

--- src/models/appliance.js
+++ src/models/appliance.js
@@ -1,3 +1,17 @@
 export default class Appliance {
+  constructor(data = []) {
+    this.$collection = [];
 
+    if (data.length) this.record(data);
+  }
+
+  record(data) {
+    this.$collection.push(...data);
+  }
+
+  all() { }
+
+  find() { }
+
+  update() { }
 }
--- src/tests/models/appliance.test.js
+++ src/tests/models/appliance.test.js
@@ -1,4 +1,6 @@
-import { describe, it, expect } from 'vitest';
+import {
+  describe, it, expect, vi,
+} from 'vitest';
 import Appliance from '../../models/appliance';
 
 describe('Appliance model', () => {
@@ -6,8 +8,27 @@ describe('Appliance model', () => {
     expect(new Appliance()).toBeInstanceOf(Appliance);
   });
   it('Appliance structure', () => {
-    expect(new Appliance()).toEqual({
+    expect(new Appliance()).toEqual(expect.objectContaining({
       $collection: expect.any(Array),
+      record: expect.any(Function),
+      all: expect.any(Function),
+      find: expect.any(Function),
+      update: expect.any(Function),
+    }));
+  });
+  describe('record', () => {
+    const data = [{ name: 'Test-appiance' }, { name: 'Test-appliance 2' }];
+    it('Can add data to the collection', () => {
+      const appliance = new Appliance();
+      appliance.record(data);
+      expect(appliance.$collection).toEqual(data);
+    });
+    it('Gets called when data is passed to Appliance', () => {
+      const spy = vi.spyOn(Appliance.prototype, 'record');
+      // eslint-disable-next-line no-unused-vars
+      const appliance = new Appliance(data);
+      expect(spy).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalledWith(data);
     });
   });
 });
